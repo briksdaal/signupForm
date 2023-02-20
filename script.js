408aadc16edd1f1b2a0b16ce022e7e0010a07cbf
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-02-20 17:43:00 +0200

    Add identical password confirmation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("check");
+form = document.querySelector("form");
+pwd = document.querySelector("#pwd");
+pwdconf = document.querySelector("#pwdconf");
+pwdError = document.querySelector(".pwd-error")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(`pwd: "${pwd.value}"`);
+    console.log(`pwdconf: "${pwdconf.value}"`);
+    if (pwd.value !== pwdconf.value) {
+        e.preventDefault();
+        pwdError.textContent = `* Passwords do not match`;
+        pwd.classList.add("invalid");
+        pwdconf.classList.add("invalid");
+    } else {
+        pwdError.textContent = ``;
+        pwd.classList.remove("invalid");
+        pwdconf.classList.remove("invalid");
+    }
+});
